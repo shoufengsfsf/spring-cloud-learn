6553e699135ba16b8905a7e225ef3e3e4ef00a72
maozhihao <915855132@qq.com>
2020-04-07 12:52:22 +0800

    feature：添加DiscoveryClient服务发现

--- eureka-learn/eureka-client-learn/eureka-client-learn-consumer-demo-01/src/main/java/com/shoufeng/eureka/client/consumer/demo01/EurekaClientConsumerDemo01Application.java
+++ eureka-learn/eureka-client-learn/eureka-client-learn-consumer-demo-01/src/main/java/com/shoufeng/eureka/client/consumer/demo01/EurekaClientConsumerDemo01Application.java
@@ -2,6 +2,7 @@ package com.shoufeng.eureka.client.consumer.demo01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,6 +11,8 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 //开启eureka client
 @EnableEurekaClient
+//开启服务发现客户端
+@EnableDiscoveryClient
 public class EurekaClientConsumerDemo01Application {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientConsumerDemo01Application.class, args);
--- eureka-learn/eureka-client-learn/eureka-client-learn-consumer-demo-01/src/main/java/com/shoufeng/eureka/client/consumer/demo01/controller/EurekaClientConsumerDemo01Controller.java
+++ eureka-learn/eureka-client-learn/eureka-client-learn-consumer-demo-01/src/main/java/com/shoufeng/eureka/client/consumer/demo01/controller/EurekaClientConsumerDemo01Controller.java
@@ -1,6 +1,7 @@
 package com.shoufeng.eureka.client.consumer.demo01.controller;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import org.springframework.web.client.RestTemplate;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author shoufeng
@@ -20,9 +24,28 @@ public class EurekaClientConsumerDemo01Controller {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/para")
     public String getRequest(HttpServletRequest request, HttpServletResponse httpServletResponse) {
         String para = request.getParameter("para");
         return restTemplate.getForEntity("http://PROVIDER-DEMO-SERVICE/provider/para", String.class).getBody();
+    }
+
+    @GetMapping("/discovery")
+    public Map<String, Object> getDiscoveryInfo() {
+        Map<String, Object> map = new HashMap<>();
+        String description = discoveryClient.description();
+        int order = discoveryClient.getOrder();
+        List<String> services = discoveryClient.getServices();
+        List<ServiceInstance> serviceInstances = discoveryClient.getInstances("provider-demo-service");
+        map.put("description", description);
+        map.put("order", order);
+        map.put("services", services);
+        map.put("serviceInstances", serviceInstances);
+
+
+        return map;
     }
 }
